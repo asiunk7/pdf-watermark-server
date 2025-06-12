467b15b534f768514daeb633789f4f8a4617e177
asiunk7 <104195267+asiunk7@users.noreply.github.com>
2025-06-12 14:38:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def generate_pdf():
     return send_file(output, as_attachment=True, download_name=f"watermarked_{filename}", mimetype='application/pdf')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ['PORT'])
     app.run(host='0.0.0.0', port=port)
